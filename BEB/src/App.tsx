08c1a1216a17d55388a9e1947064d31bb18399c4
CHAERYEON-LEE <sogogijjang@naver.com>
2024-11-21 23:33:04 +0900

    Feat: 인프라 테스트를 위한 테스트코드

--- BEB/src/App.tsx
+++ BEB/src/App.tsx
@@ -1,7 +1,25 @@
 // import { useState } from 'react';
 import './styles/main.module.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [serverData, setServerData] = useState('');
+
+  useEffect(() => {
+    const getServerData = () => {
+      axios
+        .get('/api/demo')
+        .then((res: unknown) => {
+          console.log(res);
+          setServerData(res?.data);
+        })
+        .catch((err) => console.log(err));
+    };
+
+    getServerData();
+  }, []);
+
   return (
     <>
       <div className="main">
@@ -9,6 +27,11 @@ function App() {
           <img src="src\assets\3d토끼.png" className="main_logo" alt="logo" />
         </a>
         <h3>톡기를 누르면 이동</h3>
+        <p>
+          리액트 프로젝트 띄우기 성공!!!!
+          <br />
+          서버 응답 데이터 : {serverData}
+        </p>
       </div>
     </>
   );
