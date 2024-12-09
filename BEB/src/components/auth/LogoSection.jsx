418ca35e95bbc58997756cb05ec5ab9c5d338d7a
peachez-z <rkfld0911@gmail.com>
2024-12-09 13:14:58 +0900

    Feat: 파일명 추적 오류 해결

---
+++ BEB/src/components/auth/LogoSection.jsx
@@ -0,0 +1,12 @@
+import '../../styles/Auth/LogoSection.scss';
+
+function LogoSection() {
+  return (
+    <div className="logosection">
+      <img src="/logo/3d토끼.png" className="caracter" />
+      <img src="/logo/fulltext.png" className="textlogo" />
+    </div>
+  );
+}
+
+export default LogoSection;
